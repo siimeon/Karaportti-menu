2f9aa61eaae65c3c60508ad0795f34c13e15edd1
siimeon <simo.haakana@aalto.fi>
2015-06-22 07:39:00 +0300

    Removing ISS from list because of changed format
    At the moment ISS is not supported because of format change in their web site. ISS may be supported in future if the format will be more stable

--- todays_menus.py
+++ todays_menus.py
@@ -12,25 +12,27 @@ def get_today_str():
 
 def load_menus():
     amica_menu = json_to_obj(amica_to_json())
-    iss_menu = json_to_obj(iss_to_json())
+    #iss_menu = json_to_obj(iss_to_json())
     today = get_today_str()
     amica_today = amica_menu[today]
-    iss_today = iss_menu[today]
-    return {"amica": amica_today, "iss": iss_today}
+    #iss_today = iss_menu[today]
+    return {"amica": amica_today, "iss": None}
 
 def print_menus(today_obj):
     amica = today_obj["amica"]
-    iss = today_obj["iss"]
+    #iss = today_obj["iss"]
     for dish in amica["list"]:
         if dish["Nokia Price"] is not None:
             print(dish["Name"] + " " + dish["Nokia Price"])
         else:
             print(dish["Name"])
+    """
     for dish in iss["list"]:
         if dish["Nokia Price"] is not None:
             print(dish["Name"] + " " + dish["Nokia Price"])
         else:
             print(dish["Name"])
+    """
 
 def main():
     print(obj_to_json(load_menus()))
